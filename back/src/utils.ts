81e84aeb4d2780bf0996601f4e8c4a2cb8a6bc1e
witzkvn <witz.kevin05@gmail.com>
2022-10-20 14:39:17 +0200

    changed sqlite db to postgres

--- back/src/utils.ts
+++ back/src/utils.ts
@@ -4,8 +4,12 @@ import { Grade } from "./entity/Grade";
 import { Skill } from "./entity/Skill";
 
 const dataSource = new DataSource({
-  type: "sqlite",
-  database: "./wildersdb.sqlite",
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "example",
+  database: "postgres",
   synchronize: true,
   entities: [Wilder, Skill, Grade],
   // logging: ["query", "error"],
